150b515efce9253458bf3c23dfbd66b0f1bc41ec
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2020-01-30 19:16:48 -0300

    alterações

--- api/server/src/migrations/4-create-itens.js
+++ api/server/src/migrations/4-create-itens.js
@@ -15,7 +15,7 @@ module.exports = {
         type: Sequelize.INTEGER,
       },
       statusItem: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
